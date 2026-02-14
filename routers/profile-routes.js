0295e6b06a94ca00fd5db2248cf95b74be57ae4a
KoSi <otpog23@gmail.com>
2026-02-14 23:50:15 +0000

    delete user router

--- routers/profile-routes.js
+++ routers/profile-routes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const authMiddleware = require('../middleware/auth-middleware');
-const { updateUsername, updateBio } = require('../controllers/profile');
+const { updateUsername, updateBio, deleteUser } = require('../controllers/profile');
 
 const router = express.Router();
 
 router.put('/update/username/:id', authMiddleware, updateUsername);
 
 router.put('/update/bio/:id', authMiddleware, updateBio);
+
+router.delete('/del/user/:id', authMiddleware, deleteUser);
 
 module.exports = router;
