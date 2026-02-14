ff509a01e4b982f78d51882db3b194677bdec7e7
KoSi <otpog23@gmail.com>
2026-02-14 23:39:55 +0000

    update bio router

--- routers/profile-routes.js
+++ routers/profile-routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const authMiddleware = require('../middleware/auth-middleware');
-const { updateUsername } = require('../controllers/profile');
+const { updateUsername, updateBio } = require('../controllers/profile');
 
 const router = express.Router();
 
 router.put('/update/username/:id', authMiddleware, updateUsername);
+
+router.put('/update/bio/:id', authMiddleware, updateBio);
 
 module.exports = router;
