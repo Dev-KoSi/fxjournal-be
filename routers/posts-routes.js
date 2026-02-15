6e1029abf822b31ba2db7cc494629ac8c572e00c
KoSi <otpog23@gmail.com>
2026-02-15 15:49:41 +0000

    add log to fav controller: bug fix

--- routers/posts-routes.js
+++ routers/posts-routes.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const authMiddleware = require('../middleware/auth-middleware');
 const imageMiddleware = require("../middleware/image-middleware");
-const {postLog, updateLog} = require('../controllers/post-log')
+const {postLog, updateLog, updateFav} = require('../controllers/post-log')
 
 const router = express.Router();
 
 router.post('/post/log/:id', authMiddleware, imageMiddleware, postLog);
 
 router.put('/update/caption/:id', authMiddleware, updateLog);
+
+router.put('/update/fav/:id', authMiddleware, updateFav);
 
 module.exports = router;
