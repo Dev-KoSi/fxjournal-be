4a6c666a9daa4e3b0bea0407f1ac657e7dd4fc87
KoSi <otpog23@gmail.com>
2026-02-21 11:48:10 +0000

    update profile controller & router

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,47 +1,20 @@
 const User = require('../model/user');
 
-const updateUsername = async (req, res) => {
+const updateProfile = async (req, res) => {
     try {
         const userId = req.params.id;
-        const {newUsername} = req.body;
+        const {newUsername, newBio} = req.body;
 
-        const updatedUsername = await User.findByIdAndUpdate(userId, {username: newUsername}, {new: true});
+        const updatedProfile = await User.findByIdAndUpdate(userId, {username: newUsername, bio: newBio}, {new: true});
 
-        if(updatedUsername) {
+        if(updatedProfile) {
             return res.status(200).json({
                 success: true,
-                message: `Username updated successfully.`,
-                newUsername: updatedUsername.username
-            })
-        } else {
-            return res.status(400).json({
-                success : false,
-                message : `Something went wrong, try again.`
-            })
-        }
-        
-    } catch (error) {
-        console.log(error);
-
-        res.status(500).json({
-            success : false,
-            message : `Something went wrong, try again.`
-        })
-    }
-}
-
-const updateBio = async (req, res) => {
-    try {
-        const userId = req.params.id;
-        const {newBio} = req.body;
-
-        const updatedBio = await User.findByIdAndUpdate(userId, {bio: newBio}, {new: true});
-
-        if(updatedBio) {
-            return res.status(200).json({
-                success: true,
-                message: `Bio updated successfully.`,
-                newBio: updatedBio.bio
+                message: `Profile updated successfully.`,
+                newProfile: {
+                    username: updatedProfile.username,
+                    bio: updatedProfile.bio
+                }
             })
         } else {
             return res.status(400).json({
@@ -92,4 +65,4 @@ const deleteUser = async (req, res) => {
     }
 }
 
-module.exports = {updateUsername, updateBio, deleteUser};
+module.exports = {updateProfile, deleteUser};
--- routers/profile-routes.js
+++ routers/profile-routes.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const authMiddleware = require('../middleware/auth-middleware');
-const { updateUsername, updateBio, deleteUser } = require('../controllers/profile');
+const {deleteUser, updateProfile } = require('../controllers/profile');
 
 const router = express.Router();
 
-router.put('/update/username/:id', authMiddleware, updateUsername);
-
-router.put('/update/bio/:id', authMiddleware, updateBio);
+router.put('/update/profile/:id', authMiddleware, updateProfile);
 
 router.delete('/del/user/:id', authMiddleware, deleteUser);
 
